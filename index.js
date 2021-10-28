3f107b7781d18dc51a333954599793964c772101
joybiswasmritunjoy <contact.joybiswas@gmail.com>
2021-10-29 00:43:00 +0600

    Project set up

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Use middleware 
+app.use(cors())
+app.use(express.json());
+
+
+// Initial Server 
+app.get('/', (req, res) => {
+    res.send("Server Is running")
+});
+
+app.listen(port, () => {
+    console.log("listining to",port);
+})
+
+
+
+
