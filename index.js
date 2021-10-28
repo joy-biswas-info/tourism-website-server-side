030cd0dd9d7b8a29b3d953c4fd50fe75454dcb32
joybiswasmritunjoy <contact.joybiswas@gmail.com>
2021-10-29 02:28:20 +0600

    server connect with mongo db

--- index.js
+++ index.js
@@ -10,8 +10,19 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
+// Connect with database 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0f9z2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+    } finally {
+        // await client.close();
+    }
+};
+run().catch(console.dir);
 
 // Initial Server 
 app.get('/', (req, res) => {
